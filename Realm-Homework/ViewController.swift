eb9fe004d7135deea54673c2ca18454746818d54
Andrey <andreyly@bk.ru>
2021-01-12 21:39:56 +0300

    start project

---
+++ Realm-Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm-Homework
+//
+//  Created by Андрей Лапин on 12.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
